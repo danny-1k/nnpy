4d7329680b08edc2657945626563b1b5b0406d8d
Daniel Ik <codingeinstein@gmail.com>
2021-10-09 09:58:54 +0100

    added gradient checking

--- nnpy/core/tests.py
+++ nnpy/core/tests.py
@@ -5,7 +5,7 @@ from numpy.linalg import norm
 def numerical_grad(func,x):
     '''
     Function to calculate gradients
-    numerically
+    of a function numerically
 
     f(x+h) - f(x-h)
     ---------------
@@ -13,4 +13,40 @@ def numerical_grad(func,x):
 
     '''
 
+    h = 1e-8
+
+    grad_vec = []
+
+    #assuming x is a (num,shape) matrix
+    #for example (2,3,3) matrix
+    #then we need to compute the gradient one at a time
+
+    if x.shape > 2:
+        for xi in x:
+            f_x_plus_h = func(xi+h)
+            f_x_min_h = func(xi-h)
+
+            g = (f_x_plus_h-f_x_min_h)/(2*h)
+
+            grad_vec.append(g)
+
+        grad_vec =  np.array(grad_vec).T
+
+    else:
+        f_x_plus_h = func(x+h)
+        f_x_min_h = func(x-h)
+
+        grad_vec = (f_x_plus_h-f_x_min_h)/(2*h)
+
     
+    return grad_vec
+
+
+
+def compare(num_grads,grads):
+    diff = abs(num_grads-grads) < 1e-8 
+
+    if np.sum(diff) == diff.size:
+        print('The derivated gradients are correct')
+    else:
+        print('The derivated gradients are wrong')
