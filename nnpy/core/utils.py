a22e12b66fbb06bea834922ad5e9d95372e7dbe8
Daniel Ik <codingeinstein@gmail.com>
2021-10-19 12:40:16 +0100

    updated utils.py

--- nnpy/core/utils.py
+++ nnpy/core/utils.py
@@ -2,22 +2,25 @@ def create_graph(x,layers):
     idx = 0
     g = []
     while True:
-        layer = layers[idx]
-        if layer.x is not None:
-            if layer.x.data == x.data:
-                g.append(layer)
-                x = layer.out
-                del layers[idx]
-                idx = 0
-            else:
-                if idx == len(layers):
+        try:
+            layer = layers[idx]
+            if layer.x is not None:
+                if layer.x.data == x.data:
+                    g.append(layer)
+                    x = layer.out
+                    del layers[idx]
                     idx = 0
                 else:
-                    idx+=1
+                    if idx == len(layers):
+                        idx = 0
+                    else:
+                        idx+=1
 
-            if len(layers) == 0:
+                if len(layers) == 0:
+                    break
+            else:
                 break
-        else:
+        except:
             break
         
     return g
