e903f910e082c5d35bc77128868ad5c8f93356f6
Daniel Ik <codingeinstein@gmail.com>
2021-11-02 13:54:26 +0100

    added Adam optimizer

--- nnpy/optim/optim.py
+++ nnpy/optim/optim.py
@@ -64,3 +64,43 @@ class Adagrad(Optim):
                 for item in layer.grads:
                     layer.params[item] -= layer.grads[item] * \
                         (self.lr/(np.sqrt(self.val[idx][item])+self.epsilon))
+
+
+class Adam(Optim):
+    def __init__(self,net,lr=1e-4,beta1=0.9,beta2=0.999,epsilon=1e-8):
+        super().__init__(net, lr)
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.m = {}
+        self.v = {}
+        self.t = 0
+
+        for idx,layer in enumerate(self.net.layers):
+            values = {}
+
+            if not 'x' in layer.grads and not isinstance(layer,Function):
+                for item in layer.grads:
+                    
+                    values[item] = np.zeros_like(layer.params[item])
+
+            self.v[idx] = values
+            self.m[idx] = values
+
+    
+    def step(self):
+        self.t+=1 #increment t
+
+        for idx,layer in enumerate(self.net.layers):
+
+            if not 'x' in layer.grads and not isinstance(layer,Function):
+                for item in layer.grads:
+                    self.m[idx][item] += self.beta1*self.m[idx][item] + (1-self.beta1)*layer.grads[item]
+                    self.v[idx][item] += self.beta2*self.v[idx][item] + (1-self.beta2)*(layer.grads[item]**2)
+
+                    #bias correction
+
+                    self.m[idx][item] = self.m[idx][item]/(1-self.beta1**self.t)
+                    self.v[idx][item] = self.v[idx][item]/(1-self.beta2**self.t)
+
+                    layer.params[item] -= self.lr/(self.v[idx][item]**.5 + self.epsilon) * self.m[idx][item]
