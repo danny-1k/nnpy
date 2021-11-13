2d8dfca8d188a3b275859c037511874fc51b4368
Daniel Ik <codingeinstein@gmail.com>
2021-11-13 15:51:57 +0100

    added RMSProp optimizer

--- nnpy/optim/optim.py
+++ nnpy/optim/optim.py
@@ -2,7 +2,7 @@ import numpy as np
 from ..core.base import Optim,Function
 
 class SGD(Optim):
-    def __init__(self,net,lr,momentum=None):
+    def __init__(self,net,lr=1e-2,momentum=None):
         '''
         Stochastic gradient descent updates the parameters
         of the network by multiplying the gradients by 
@@ -46,14 +46,20 @@ class Adagrad(Optim):
         self.epsilon = epsilon
         self.g = {}
         self.val = {}
-        for idx,layer in enumerate(self.net.layers):
-            val = {}
-            if not 'x' in layer.grads and not isinstance(layer,Function) :
-                for item in layer.grads:
-                    val[item] = np.zeros_like(layer.params[item])
-            self.val[idx] = val
     
     def step(self):
+
+        #when first self.step() is called the first time,
+        #instantiate all necessary vals
+        
+        if self.g=={} and self.val == {}:
+            for idx,layer in enumerate(self.net.layers):
+                val = {}
+                if not 'x' in layer.grads and not isinstance(layer,Function) :
+                    for item in layer.grads:
+                        val[item] = np.zeros_like(layer.params[item])
+                self.val[idx] = val
+
         for idx,layer in enumerate(self.net.layers):
             if not 'x' in layer.grads and not isinstance(layer,Function) :
                 for item in layer.grads:
@@ -64,6 +70,34 @@ class Adagrad(Optim):
                 for item in layer.grads:
                     layer.params[item] -= layer.grads[item] * \
                         (self.lr/(np.sqrt(self.val[idx][item])+self.epsilon))
+
+class RMSProp(Optim):
+    def __init__(self, net, lr,beta=.9,epsilon=1e-8):
+        self.v = {}
+        self.beta = beta
+        self.epsilon = epsilon
+        self.lr = lr
+        self.net = net
+
+    def step(self):
+        if self.v == {}:
+            for idx,layer in enumerate(self.net.layers):
+                v = {}
+                if not 'x' in layer.grads and not isinstance(layer,Function) :
+                    for item in layer.grads:
+                        v[item] = np.zeros_like(layer.params[item])
+                self.val[idx] = v
+
+        
+        for idx,layer in enumerate(self.net.layers):
+            if not 'x' in layer.grads and not isinstance(layer,Function):
+                for item in layer.grads:
+                    self.v[idx][item] += self.beta*self.v[idx][item] + (1-self.beta) * (layer.grads[item]**2)
+
+        for idx,layer in enumerate(self.net.layers):
+            if not 'x' in layer.grads and not isinstance(layer,Function):
+                for item in layer.grads:
+                    self.params[item] -= self.lr/(self.v[idx][item]**.5) * layer.grads[item]
 
 
 class Adam(Optim):
@@ -95,12 +129,20 @@ class Adam(Optim):
 
             if not 'x' in layer.grads and not isinstance(layer,Function):
                 for item in layer.grads:
-                    self.m[idx][item] += self.beta1*self.m[idx][item] + (1-self.beta1)*layer.grads[item]
-                    self.v[idx][item] += self.beta2*self.v[idx][item] + (1-self.beta2)*(layer.grads[item]**2)
+
+                    self.v[idx][item] = self.beta1*self.m[idx][item] + (1-self.beta1)*layer.grads[item]
+                    self.m[idx][item] = self.beta2*self.v[idx][item] + (1-self.beta2)*np.square(layer.grads[item])
 
                     #bias correction
 
-                    self.m[idx][item] = self.m[idx][item]/(1-self.beta1**self.t)
-                    self.v[idx][item] = self.v[idx][item]/(1-self.beta2**self.t)
+                    v_hat = self.m[idx][item]/(1-pow(self.beta1,self.t))
+                    m_hat = self.v[idx][item]/(1-pow(self.beta2,self.t))
 
-                    layer.params[item] -= self.lr/(self.v[idx][item]**.5 + self.epsilon) * self.m[idx][item]
+                    print(m_hat)
+                    print('m_hat')
+                    print(self.m[idx][item])
+                    print('m')
+                    print(layer.grads[item]**2)
+                    print('Squared grads')
+
+                    layer.params[item] -= self.lr * (v_hat/(np.sqrt(m_hat) + self.epsilon))
