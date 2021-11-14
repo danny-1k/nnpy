6e5ff21b68cf3af8a9a08630eac4811609715b68
Daniel Ik <codingeinstein@gmail.com>
2021-11-14 12:57:35 +0100

    updated test.py

--- mnist/test.py
+++ mnist/test.py
@@ -4,7 +4,7 @@ sys.path.append('..')
 from PIL import Image
 import numpy as np
 
-from model import net
+from model import FC
 
 def transform(x,ops):
     for op in ops:
@@ -18,20 +18,7 @@ transforms = [
     lambda x:x.reshape(1,28*28),
 ]
 
-w1 = np.load('weights/w1.npy')
-w2 = np.load('weights/w2.npy')
-b1 = np.load('weights/b1.npy')
-b2 = np.load('weights/b2.npy')
-w3 = np.load('weights/w3.npy')
-b3 = np.load('weights/b3.npy')
-
-net.layers[0].params['w'] = w1
-net.layers[0].params['b'] = b1
-net.layers[2].params['w'] = w2
-net.layers[2].params['b'] = b2
-net.layers[4].params['w'] = w3
-net.layers[4].params['b'] = b3
-
+net = FC()
 net.eval()
 
 labels = ['0','1','2','3','4','5','6','7','8','9']
