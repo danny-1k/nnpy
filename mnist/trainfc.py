ffeb35c95e1f22d38f3b45fe1d4f1d5378edf17d
Daniel Ik <codingeinstein@gmail.com>
2021-11-14 15:17:58 +0100

    nothin

--- mnist/trainfc.py
+++ mnist/trainfc.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('..')
 
 import nnpy.nn as nn
-from nnpy.optim import Adam
+from nnpy.optim import Adam,SGD
 
 from model import FC
 
@@ -21,6 +21,12 @@ trans = transforms.Compose(
 
 net = FC()
 
+# try:
+#     net.load_weights('FC')
+#     print('loaded weights')
+# except:
+#     pass
+
 train = datasets.MNIST('~', train=True, transform=trans)
 test = datasets.MNIST('~', train=False, transform=trans)
 
@@ -28,10 +34,10 @@ train_loader = DataLoader(train, batch_size=32, shuffle=True)
 test_loader = DataLoader(test, batch_size=32, shuffle=True)
 
 epochs = 15
-lr = 1e-5
+lr = 1e-3
 
 lossfn = nn.CrossEntropy(net)
-optim = Adam(net,lr=lr)
+optim = SGD(net,lr=lr)
 
 net.train_on(optimizer=optim,
         lossfn=lossfn,
