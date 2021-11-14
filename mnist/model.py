a374863cf834e10df99f24a0c205427ddb3a1fd5
Daniel Ik <codingeinstein@gmail.com>
2021-11-14 14:23:34 +0100

    updated model.py

--- mnist/model.py
+++ mnist/model.py
@@ -1,9 +1,110 @@
 import os
 import numpy as np
 import nnpy.nn as nn
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+
+class Base:
+
+    def save_weights(self,save_weights_in):
+        if save_weights_in not in os.listdir('weights'):
+            print('Weights folder does not exist. Creating')
+            os.makedirs(f'weights/{save_weights_in}')
+
+        
+        for layer_idx,layer in enumerate(self.layers):
+            for param in layer.params:
+                np.save(f'weights/{save_weights_in}/layeridx_{layer_idx}_param_{param}',layer.params[param])
+    
+    def load_weights(self,save_weights_in):
+        if save_weights_in not in os.listdir('weights'):
+            print('Weights folder does not exist. Creating')
+            os.makedirs(f'weights/{save_weights_in}')
+
+        for layer_idx,layer in enumerate(self.layers):
+            for param in layer.param:
+                param_file = f'layeridx_{layer_idx}_param_{param}'
+                if param_file not in os.listdir(f'weights/{save_weights_in}'):
+                    print(f'Param for {param} in layer {layer}({layer_idx}) not found')
+                self.layers[layer_idx][param] = np.load(f'weights/{save_weights_in}/{param_file}')
+
+    
+    def train(self,optimizer,lossfn,trainloader,testloader,save_weights_in,plots_folder='plots',epochs=10,):
+        
+        test_loss_over_time = []
+        train_loss_over_time = []
+        acc_over_time = [0]
 
 
-class FC(nn.Module):
+        for epoch in tqdm(range(epochs)):
+            train_batch_loss = []
+            test_batch_loss = []
+            accuracy = 0
+
+            self.train()
+
+            for x,y in trainloader:
+                x = x.view(-1,28*28).numpy()
+                y = y.numpy()
+
+                p = self.__call__(x)
+
+                loss = lossfn(p,y)
+
+                train_batch_loss.append(loss)
+
+                lossfn.backward()
+
+                optimizer.step()
+                optimizer.zero_grad()
+
+
+            self.eval()
+
+            for x,y in testloader:
+                x = x.view(-1,28*28).numpy()
+                y = y.numpy()
+
+                p = self.__call__(x)
+
+                loss = lossfn(p,y)
+
+                test_batch_loss.append(loss)
+
+                accuracy += sum(p.argmax(axis=1) == y)
+            
+            accuracy = accuracy/len(testloader.dataset)
+            acc_over_time.append(accuracy)
+
+            train_loss_over_time.append(np.mean(train_batch_loss))
+            test_loss_over_time.append(np.mean(test_batch_loss))
+
+            if acc_over_time[-1] > acc_over_time[-2]:
+                self.save_weights(save_weights_in)
+
+
+            plt.plot(train_loss_over_time,legend='train')
+            plt.plot(test_loss_over_time,legend='test')
+
+            plt.legend()
+
+            plt.savefig(os.path.join(self.plots_folder,'loss.png'))
+            
+            plt.close('all')
+
+
+            plt.plot(acc_over_time,legend='accuracy')
+
+            plt.legend()
+
+            plt.savefig(os.path.join(plots_folder,'accuracy.png'))
+            
+            plt.close('all')
+
+
+
+class FC(nn.Module,Base):
     def __init__(self):
 
         self.fc1 = nn.Linear(28*28,128)
@@ -28,27 +129,3 @@ class FC(nn.Module):
         x = self.soft(self.fc3(x))
 
         return x
-
-    
-    def save_weights(self):
-        if 'fc_model' not in os.listdir('weights'):
-            print('Weights folder does not exist. Creating')
-            os.makedirs('weights/fc_model')
-
-        
-        for layer_idx,layer in enumerate(self.layers):
-            for param in layer.params:
-                np.save(f'weights/fc_model/layeridx_{layer_idx}_param_{param}',layer.params[param])
-    
-
-    def load_weights(self):
-        if 'fc_model' not in os.listdir('weights'):
-            print('Weights folder does not exist. Creating')
-            os.makedirs('weights/fc_model')
-
-        for layer_idx,layer in enumerate(self.layers):
-            for param in layer.param:
-                param_file = f'layeridx_{layer_idx}_param_{param}'
-                if param_file not in os.listdir('weights/fc_model'):
-                    print(f'Param for {param} in layer {layer}({layer_idx}) not found')
-                self.layers[layer_idx][param] = np.load(f'weights/fc_model/{param_file}')
